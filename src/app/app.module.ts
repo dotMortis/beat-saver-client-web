45bc3b0e2461224d3662feab086ca7bd0659eaa4
Nick K <29275165+dotMortis@users.noreply.github.com>
2021-09-27 19:23:40 +0200

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RippleModule } from 'primeng/ripple';
+import { TooltipModule } from 'primeng/tooltip';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, NgbModule, BrowserAnimationsModule, FontAwesomeModule],
+    imports: [
+        BrowserModule,
+        NgbModule,
+        BrowserAnimationsModule,
+        FontAwesomeModule,
+        RippleModule,
+        TooltipModule
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule {
     constructor(private library: FaIconLibrary) {
-        library.addIcons();
+        library.addIcons(faGithub, faGithubSquare);
     }
 }
