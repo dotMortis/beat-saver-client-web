91d0172eaf0e2ba9a84506d2f9573cbef64d29c9
Nick K <29275165+dotMortis@users.noreply.github.com>
2021-09-27 18:23:53 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# BeatSaverClient Website
+
+### Download latest client [here](https://github.com/dotMortis/beat-saver-client/releases)!
+\
+[!["Become a Patreon"](src/assets/become_a_patreon.png)](https://www.patreon.com/dotmortis)&nbsp;&nbsp;&nbsp;&nbsp;[!["Buy Me A Coffee"](src/assets/coffee.png)](https://ko-fi.com/dotmortis)
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'beat-saver-client-web'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('beat-saver-client-web');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('beat-saver-client-web app is running!');
+  });
+});
